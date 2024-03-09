464c59e03ff7e7eee5b2e1be6eb51b84be10c2c4
dalopea <d.lopez.william@gmail.com>
2024-03-09 15:15:27 +0100

    Exoplayer que es mucho más simple, generalizado, con 3 ejemplos y obligado en landscape

--- app/src/main/java/com/example/filmnitebp/MainActivity.kt
+++ app/src/main/java/com/example/filmnitebp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.filmnitebp
 
 
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,16 +24,22 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import com.example.filmnitebp.ui.theme.FilmNiteBPTheme
 
 
 
+val url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+val url2 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+val url3="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             FilmNiteBPTheme {
                 // A surface container using the 'background' color from the theme
@@ -34,25 +47,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    MainScreen(url)
                 }
             }
         }
     }
 }
 
-@Composable
-fun MainScreen(){
+@OptIn(UnstableApi::class) @Composable
+fun MainScreen(url:String){
     val context= LocalContext.current
-    val url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+
     val exoPlayer= ExoPlayer.Builder(context).build()
     val mediaItem= MediaItem.fromUri(Uri.parse(url))
     exoPlayer.setMediaItem(mediaItem)
+    Screen()
 
 
     val playerView=PlayerView(context)
+    playerView.systemUiVisibility
     playerView.player=exoPlayer
-    DisposableEffect(AndroidView(factory = {playerView}) ){
+    DisposableEffect(AndroidView(factory = {playerView.apply {
+        player=exoPlayer
+        useController=true
+        resizeMode=AspectRatioFrameLayout.RESIZE_MODE_FILL
+        layoutParams=FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    }}) ){
+
 
         exoPlayer.prepare()
         exoPlayer.playWhenReady=true
@@ -62,4 +86,26 @@ fun MainScreen(){
         }
     }
 
+}
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+@Composable
+fun Screen() {
+    LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 }
