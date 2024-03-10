fbfc2089bfbccea8c9f75eb196ae5b0a3abe6c30
dalopea <d.lopez.william@gmail.com>
2024-03-10 13:48:06 +0100

    Video player sin dos botones inutiles, además si deslizas arriba salen los controles y si deslizas hacia abajo con los controles puestos, estos se van

--- app/src/main/java/com/example/filmnitebp/MainActivity.kt
+++ app/src/main/java/com/example/filmnitebp/MainActivity.kt
@@ -8,22 +8,38 @@ import android.content.ContextWrapper
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import android.widget.FrameLayout
+import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
@@ -37,8 +53,15 @@ val url2 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Ele
 val url3="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
 
 class MainActivity : ComponentActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Estas 2 funciones sirven para que la status bar no aparezca de forma que se vea perfecto el video.
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         setContent {
             FilmNiteBPTheme {
@@ -54,21 +77,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(UnstableApi::class) @Composable
+
+//Esto es  lo relacionado con el videoplayer, la idea seria moverlo a su propia clase, por ahora solo eran pruebas de funcionamiento
+@OptIn(UnstableApi::class)
+@Composable
 fun MainScreen(url:String){
     val context= LocalContext.current
 
     val exoPlayer= ExoPlayer.Builder(context).build()
     val mediaItem= MediaItem.fromUri(Uri.parse(url))
     exoPlayer.setMediaItem(mediaItem)
-    Screen()
+
 
 
     val playerView=PlayerView(context)
-    playerView.systemUiVisibility
+    var controllerStatus=false
+
+
+    Screen()
     playerView.player=exoPlayer
+
+    //Quitamos el boton de previous y next
+
+
+   // playerView.setShowRewindButton(false)
+   // playerView.setShowFastForwardButton(false)
    // val inicio=exoPlayer.currentPosition //ver el moment actual
     // exoPlayer.seekTo(50000)   //Prueba para ver el seekto
+
+    var offsetY by remember { mutableStateOf(0f) }
     DisposableEffect(AndroidView(factory = {playerView.apply {
         player=exoPlayer
         useController=true
@@ -77,11 +114,58 @@ fun MainScreen(url:String){
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT
         )
-    }}) ){
+    }
+        },Modifier.pointerInput(Unit){
+
+        detectVerticalDragGestures(onVerticalDrag = {change, dragAmount ->
+            change.consume()
+            offsetY=dragAmount//vemos cuanto se ha movido, si es positivo se ha movido hacia abajo, si es negatio hacia arriba
+
+        },
+            onDragStart = {offset ->
+                //offsetY=offset.y
+            },onDragEnd = {
+
+                if(offsetY<0){
+                    if(!controllerStatus) {
+                        playerView.useController = true
+                        playerView.showController()
+                        controllerStatus = true
+                    }else{
+                        Log.d("User Input", "Drag end arriba @ ${offsetY}") //esto es para ver el funcionamiento
+                    }
+                }else{
+                    if(controllerStatus){
+                        playerView.hideController()
+                        playerView.useController=false
+                        controllerStatus=false
+                    }else{
+                        Log.d("User Input", "Drag end abajo @ ${offsetY}")//esto es para ver el funcionamiento
+                    }
+
+                }
+
+                                         }, onDragCancel = {})
+
+        detectTapGestures(onTap = {}, onDoubleTap = {},
+            onLongPress = {}, onPress = {})//este no me funciona, a ver si alguien puede hacer algo
+
+
+    }
+
+    ) ){
 
 
         exoPlayer.prepare()
+        playerView.setShowPreviousButton(false)
+        playerView.setShowNextButton(false)
+        //Para quitar antes y despues
+        playerView.useController=false //Con esto no aparecen los controles, de forma que cuando hagamos que se deslize hacia arriba, aparecen.
+        playerView.controllerHideOnTouch=false//Evitar que se quiten los controles con el toque
+        playerView.controllerShowTimeoutMs=0//Nunca se van los ocntroles si estan en pantalla, de tal forma que haya que deslizar hacia abajo para quitarlos
         exoPlayer.playWhenReady=true
+
+
 
         onDispose {
             exoPlayer.release()
@@ -97,6 +181,7 @@ fun Context.findActivity(): Activity? = when (this) {
 @Composable
 fun LockScreenOrientation(orientation: Int) {
     val context = LocalContext.current
+
     DisposableEffect(Unit) {
         val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
         val originalOrientation = activity.requestedOrientation
@@ -110,4 +195,6 @@ fun LockScreenOrientation(orientation: Int) {
 @Composable
 fun Screen() {
     LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
 }
+
