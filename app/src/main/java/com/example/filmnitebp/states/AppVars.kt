d6c7b229305e5d7a6edfb4e004793bed4e83d18e
yediaz <yediaz@unirioja.es>
2024-03-20 17:47:46 +0100

    Group application-wide variables into a singleton class.

---
+++ app/src/main/java/com/example/filmnitebp/states/AppVars.kt
@@ -0,0 +1,10 @@
+package com.example.filmnitebp.states
+
+import com.example.filmnitebp.model.Sala
+
+class AppVars {
+    companion object {
+        var url = ""
+        lateinit var streamItem: Sala.Stream
+    }
+}
